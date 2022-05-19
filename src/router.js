3ffa9526d3318204acb23bce674eb0810d5bc30a
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2022-05-19 10:58:11 +0200

    Conf

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: 'http://localhost:8080',
+  base: 'https://geoffreyaldebert.github.io/dashboard-of/',
   routes: [
     {
       path: '/',
